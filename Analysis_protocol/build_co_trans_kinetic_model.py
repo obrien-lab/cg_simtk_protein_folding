bd5545fd46b78f4092fd5d9320eae30aa615651d
Yang Jiang <yuj179@psu.edu>
2020-09-21 22:15:05 -0400

    Update build_co_trans_kinetic_model.py

--- Analysis_protocol/build_co_trans_kinetic_model.py
+++ Analysis_protocol/build_co_trans_kinetic_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys, getopt, math, os, multiprocessing, time, traceback
+import sys, getopt, math, os, time, traceback
 import numpy as np
 import pyemma as pem
 import parmed as pmd
